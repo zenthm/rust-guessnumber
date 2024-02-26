4499e871b2385982d1ff9acdf8ca5493721c1aea
Zenthm <zenthm.dev@gmail.com>
2024-02-26 11:18:07 +0800

    Add loop for guessing

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ use std::io;
 
 fn main() {
     let number: u8 = rand::thread_rng().gen_range(u8::MIN..=u8::MAX);
-    let mut guess = String::new();
 
-    println!("Guess ({} - {}): ", u8::MIN, u8::MAX);
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Guess ({} - {}): ", u8::MIN, u8::MAX);
 
-    let guess: u8 = guess.trim().parse().expect("Your guess is not valid!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Equal => println!("You got it!"),
-        Ordering::Greater => println!("Too big!"),
+        let guess: u8 = guess.trim().parse().expect("Your guess is not valid!");
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => println!("You got it!"),
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
